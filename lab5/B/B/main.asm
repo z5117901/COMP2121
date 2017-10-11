281e57582f55d58391befba65e2b5518edbdffce
z5116011-Otto <30608520+z5116011-Otto@users.noreply.github.com>
2017-10-11 13:50:15 +1100

    j

---
+++ lab5/B/B/main.asm
@@ -0,0 +1,12 @@
+;
+; B.asm
+;
+; Created: 11/10/2017 1:42:15 PM
+; Author : ottof
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
---
+++ lab5/a/a/main.asm
@@ -0,0 +1,14 @@
+;
+; a.asm
+;
+;Detecting Motor Speed
+
+; Created: 11/10/2017 1:41:50 PM
+; Author : ottof
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
