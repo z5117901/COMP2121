3df56eba543e9943dccb633ca25a5ecc1fa58db8
z5116011-Otto <30608520+z5116011-Otto@users.noreply.github.com>
2017-10-13 15:14:18 +1100

    more

--- lab5/a/a/main.asm
+++ lab5/a/a/main.asm
@@ -11,32 +11,141 @@
 ;;Need a timer interrupt to display speed on the LCD every 100ms
 ;;Need to set how to output to LCD
 
-.macro
-
-
-
 .include "m2560def.inc"
 
-.cseg
-.org 0x0000		; Reset vector is 0x0000
-jmp RESET		; jump to start of reset handler
-.org INT0addr	; this is the address of int0 defined by the include
-jmp IRQ0		; jumps to start of handler for irq0
-.org INT2addr   ; defined address
-reti			; return to break point where int1 occured	
+;Contsants for delay
+.equ F_CPU = 16000000
+.equ DELAY_1MS = F_CPU / 4 / 1000 - 4
+/////////////////////////////////////////////////
+
+;; LCD STUFF
+.equ LCD_RS = 7
+.equ LCD_E = 6
+.equ LCD_RW = 5
+.equ LCD_BE = 4
+
+.macro do_lcd_command
+	ldi r21, @0
+	rcall lcd_command
+	rcall lcd_wait
+.endmacro
+
+.macro do_lcd_data
+	mov r22, @0
+	rcall lcd_data
+	rcall lcd_wait
+.endmacro
+
+.macro lcd_set
+	sbi PORTA, @0
+.endmacro
+
+.macro lcd_clr
+	cbi PORTA, @0
+.endmacro
+/////////////////////////////////////////////////
+
+;Basic register naming
+.def temp = r16
+.def numberL = r17
+.def numberH = r18
+/////////////////////////////////////////////////
+
+.cseg	;;; Got this table from lecture slides
+; Vector Table
+.org 0x0000
+	jmp RESET
+	jmp DEFAULT						; IRQ0 Handler
+	jmp DEFAULT						; IRQ1 Handler
+	jmp INTERRUPT2 					; IRQ2 Handler
+	jmp DEFAULT 					; IRQ3 Handler
+	jmp DEFAULT 					; IRQ4 Handler
+	jmp DEFAULT 					; IRQ5 Handler
+	jmp DEFAULT 					; IRQ6 Handler
+	jmp DEFAULT 					; IRQ7 Handler
+	jmp DEFAULT 					; Pin Change Interrupt Request 0
+	jmp DEFAULT 					; Pin Change Interrupt Request 1
+	jmp DEFAULT 					; Pin Change Interrupt Request 2
+	jmp DEFAULT 					; Watchdog Time-out Interrupt
+	jmp DEFAULT 					; Timer/Counter2 Compare Match A
+	jmp DEFAULT 					; Timer/Counter2 Compare Match B
+	jmp DEFAULT 					; Timer/Counter2 Overflow
+	jmp DEFAULT 					; Timer/Counter1 Capture Event
+	jmp UPDATE_LCD 					; Timer/Counter1 Compare Match A
+	jmp DEFAULT 					; Timer/Counter1 Compare Match B
+	jmp DEFAULT 					; Timer/Counter1 Compare Match C
+	jmp DEFAULT 					; Timer/Counter1 Overflow
+	jmp DEFAULT 					; Timer/Counter0 Compare Match A
+	jmp DEFAULT 					; Timer/Counter0 Compare Match B
+	jmp Timer0OVF 					; Timer/Counter0 Overflow
+	jmp DEFAULT 					; SPI Serial Transfer Complete
+	jmp DEFAULT 					; USART0, Rx Complete
+	jmp DEFAULT 					; USART0 Data register Empty
+	jmp DEFAULT 					; USART0, Tx Complete
+	jmp DEFAULT 					; Analog Comparator
+	jmp DEFAULT 					; ADC Conversion Complete
+	jmp DEFAULT 					; EEPROM Ready
+	jmp DEFAULT 					; Timer/Counter3 Capture Event
+	jmp DEFAULT 					; Timer/Counter3 Compare Match A
+	jmp DEFAULT 					; Timer/Counter3 Compare Match B
+	jmp DEFAULT 					; Timer/Counter3 Compare Match C
+	jmp DEFAULT 					; Timer/Counter3 Overflow
+.org 0x0072
+DEFAULT:
+	reti							; used for interrupts that are not handled
 
 RESET: 
-ldi temp, high(RAMEND)	; initialization of SP
-out SPH, temp
-ldi temp, low(RAMEND)
-out SPL, temp
+	//Stack initialization
+	ldi temp, high(RAMEND)	
+	out SPH, temp
+	ldi temp, low(RAMEND)
+	out SPL, temp
 
-rjmp main
+	//Setting outputs
+	ser temp
+	out DDRF, temp;out
+	out DDRA, temp;out
+	clr temp
+	out PORTF, temp
+	out PORTA, temp
+
+	//Sets port D int2 to an input
+	clr temp
+	out DDRD, r16 
+	
+	//THIS allows int2 to trigger falling edges which occurs when a 
+	//hole occurs in the motor wheel. Seen this in example code
+	// still to learn what isc20 &  eimsk are.
+	//External Interrupt Control Register A
+	ldi temp, (2 <<ISC20)		
+	sts EICRA, temp
+	ldi temp, (1 << INT2)
+	out EIMSK, temp			
+
+	// LCD Commands
+	do_lcd_command 0b00111000 ; 2x5x7; 0b001 DL N F x x;function set
+	rcall sleep_5ms
+	do_lcd_command 0b00111000 ; 2x5x7
+	rcall sleep_1ms
+	do_lcd_command 0b00111000 ; 2x5x7
+	do_lcd_command 0b00111000 ; 2x5x7
+	do_lcd_command 0b00001000 ; display off?
+	do_lcd_command 0b00000001 ; clear display
+	do_lcd_command 0b00000110 ; increment, no display shift;entry mode
+	do_lcd_command 0b00001110 ; display on,Cursor off, no blink
+	do_lcd_command 0b00011000
+	
+
+	//
+	sei		//setting interupt flag
+	rjmp main
+
+end:
+	jmp end
+
+main:
 
 ;;Interupt1 stuff
-
-jmp INT2 ;;IRQ2 handgler
-
 INTERRUPT2:
 	;prologue
 	push temp
@@ -49,3 +158,42 @@ INTERRUPT2:
 	pop temp
 	reti
 
+
+
+
+//Delay functions taken from previous lab
+sleep_1ms:
+	push r24
+	push r25
+	ldi r25, high(DELAY_1MS)
+	ldi r24, low(DELAY_1MS)
+
+delayloop_1ms:
+	sbiw r25:r24, 1
+	brne delayloop_1ms
+	pop r25
+	pop r24
+	ret
+
+sleep_5ms:
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	rcall sleep_1ms
+	ret
+
+sleep_25ms:
+	rcall sleep_5ms
+	rcall sleep_5ms
+	rcall sleep_5ms
+	rcall sleep_5ms
+	rcall sleep_5ms
+	ret
+
+sleep_100ms:
+	rcall sleep_25ms
+	rcall sleep_25ms
+	rcall sleep_25ms
+	rcall sleep_25ms
+	ret
