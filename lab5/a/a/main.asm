ebd02fa7ce56d5b22bf5c9c28f9968323bb8024d
z5116011-Otto <30608520+z5116011-Otto@users.noreply.github.com>
2017-10-13 14:41:27 +1100

    started adding interrupts

--- lab5/a/a/main.asm
+++ lab5/a/a/main.asm
@@ -11,10 +11,41 @@
 ;;Need a timer interrupt to display speed on the LCD every 100ms
 ;;Need to set how to output to LCD
 
+.macro
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
 
+
+.include "m2560def.inc"
+
+.cseg
+.org 0x0000		; Reset vector is 0x0000
+jmp RESET		; jump to start of reset handler
+.org INT0addr	; this is the address of int0 defined by the include
+jmp IRQ0		; jumps to start of handler for irq0
+.org INT2addr   ; defined address
+reti			; return to break point where int1 occured	
+
+RESET: 
+ldi temp, high(RAMEND)	; initialization of SP
+out SPH, temp
+ldi temp, low(RAMEND)
+out SPL, temp
+
+rjmp main
+
+;;Interupt1 stuff
+
+jmp INT2 ;;IRQ2 handgler
+
+INTERRUPT2:
+	;prologue
+	push temp
+	;body
+	ldi temp, 1
+	add numberLow ,temp
+	ldi temp, 0
+	adc numberHigh, temp
+	;epilogue
+	pop temp
+	reti
 
