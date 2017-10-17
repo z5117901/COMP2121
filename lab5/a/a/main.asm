c745208ca26ce29f63d0a6f37f5986004017348b
z5117901 <30609420+z5117901@users.noreply.github.com>
2017-10-17 14:01:26 +1100

    B is starting to work

--- lab5/a/a/main.asm
+++ lab5/a/a/main.asm
@@ -179,6 +179,19 @@ RESET:
 	
 	clr fourCounter
 	//
+
+	ldi temp, 0b00001000
+	sts DDRL, temp ; set PL3 (OC5A) as output.
+	ldi temp, 0xFF ; this value and the operation mode determine the PWM duty cycle
+	sts OCR5AL, temp
+	clr temp
+	sts OCR5AH, temp
+	ldi temp, (1 << CS50) ; CS50=1: no prescaling
+	sts TCCR5B, temp
+	ldi temp, (1<< WGM50)|(1<<COM5A1)
+	; WGM50=1: phase correct PWM, 8 bits
+	; COM5A1=1: make OC5A override the normal port functionality of the I/O pin PL3
+	sts TCCR5A, temp
 	sei		//setting interupt flag
 
 //just waiting for interupts now
